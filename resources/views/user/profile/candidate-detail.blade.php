8a40a2bd01f422ce5e18cf4534f2b5735f81fcb4
abusayed248 <abusayed20012022@gmail.com>
2024-06-07 17:53:09 +0600

    url changed

--- resources/views/user/profile/candidate-detail.blade.php
+++ resources/views/user/profile/candidate-detail.blade.php
@@ -60,7 +60,7 @@
 
                             </div>
                         </div>
-                        @if(Auth::check())  
+                        @if(Request::url() === 'https://afreel.com/candidate-detail')  
                         <div class="col-md-3 d-flex justify-content-end align-items-center">
                             <a class="applied_job2 w-100 h-100" href="{{ route('user.sub') }}">
                                 <div class=" pt-3 pb-3">
